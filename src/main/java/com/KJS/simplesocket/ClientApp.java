5de3880988ce938de8989e765d40204d44a4fa02
whatnameshouldido <pobyjun@naver.com>
2024-10-28 19:16:07 +0900

    다른 사람과 소켓 연결 및 데이터 전송 테스트
    다른 사람과 소켓 연결 및 데이터 전송 테스트

--- src/main/java/com/KJS/simplesocket/ClientApp.java
+++ src/main/java/com/KJS/simplesocket/ClientApp.java
@@ -1,23 +1,31 @@
 package com.KJS.simplesocket;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 public class ClientApp {
     private final static int port = 33333;
-    private final static String serverIp = "127.0.0.1";
+    private final static String serverIp = "192.168.0.6";
 
     public static void main(String[] args) {
-        // 클라이언트 소켓을 만든다. (IP 주소, 포트번호) (clientSocket)
+        // 클라이언트 소켓을 만든다. (IP주소, 포트번호) (clientSocket)
         // 클라이언트 소켓으로 서버에 접속 시도 한다.
         // 서버와 연결된 클라이언트 소켓으로 읽거나 쓴다.
         // 읽을때는 동기상태 (블로킹)
 
         try {
             Socket clientSocket = init();
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-            writer.write(String.format("클라이언트[%s] 접속함", serverIp));
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(clientSocket.getOutputStream())
+            );
+            writer.write(String.format("클라이언트[%s]에서 문자열 전송함", serverIp));
+            writer.newLine();
             writer.flush();
+            System.out.println("서버에 문자열 전송");
+
             writer.close();
             clientSocket.close();
         } catch (IOException ioE) {
@@ -26,11 +34,13 @@ public class ClientApp {
         } catch (Exception ex) {
             System.out.println("Exception");
             System.out.println(ex.toString());
+        } finally {
+            System.out.println("클라이언트 프로그램 종료");
         }
     }
     public static Socket init() throws IOException {
         Socket socket = new Socket(serverIp, port);
+        System.out.println("클라이언트 소켓 생성후 서버에 접속 성공");
         return socket;
     }
-
 }
--- src/main/java/com/KJS/simplesocket/ServerApp.java
+++ src/main/java/com/KJS/simplesocket/ServerApp.java
@@ -7,23 +7,26 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerApp {
-    private final static int port = 33333; // 0~9999 까지는 대부분 프로그램이 사용한다.
-                                           // 그래서 1만번 이상 하는게 좋다. 10000 ~ 65534
+    private final static int port = 33333;
+    // 0~9999 까지는 대부분 프로그램이 사용한다.
+    // 그래서 1만번이상 하는게 좋다. 10000 ~ 65534
 
     private static ServerSocket serverSocket = null;
 
     public static void main(String[] args) {
-
-        // 서버 소켓을 만든다. (listenSocket)
-        // 포트를 지정하고 bind, listen 으로 클라이언트 접속 할 때까지 대기한다. (
+        // 서버 소켓을 만든다. (serverSocket)
+        // 포트를 지정하고 bind, listen 으로 클라이언트 접속 할때까지 대기한다. (
         // 블로킹 상태) 동기상태
-        // 클라이언트 접속이 되면 클라이언트와 통신할 소켓을 리턴한다. (acceptSocket)
-        // 클라이언트 소켓으로 읽거나 쓴다, 읽을때는 동기상태 (블로킹)
+        // 클라이언트 로부터 접속이 되면 클라이언트와 연결할 소켓을 리턴하다. (acceptSocket)
+        // 클라이언트와 연결된 소켓으로 읽거나 쓴다. 읽을때는 동기상태 (블로킹)
         try {
             Socket acceptSocket = init();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(acceptSocket.getInputStream()));
-            String msg = reader.readLine();
-            System.out.printf("%s%n", msg);
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(acceptSocket.getInputStream())
+            );
+            String msg = reader.readLine(); // 블로킹 상태
+            System.out.printf("서버가 받은 메시지 : %s%n", msg);
+
             reader.close();
             acceptSocket.close();
         } catch (IOException ioE) {
@@ -32,14 +35,16 @@ public class ServerApp {
         } catch (Exception ex) {
             System.out.println("Exception");
             System.out.println(ex.toString());
+        } finally {
+            System.out.println("서버 프로그램 종료");
         }
     }
 
     public static Socket init() throws IOException {
         serverSocket = new ServerSocket(port);
         System.out.println("서버소켓으로 클라이언트 접속 대기 중");
-        Socket socket = serverSocket.accept();
-        System.out.println("클라이언트 연결됨");
+        Socket socket = serverSocket.accept(); // 블로킹 상태
+        System.out.println("클라이언트 연결 됨");
         return socket;
     }
 }
